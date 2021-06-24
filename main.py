cc652c5cf7c8d27a17895b0044ee0f4781f79c5f
Bachalo <62430254+Bachalo@users.noreply.github.com>
2021-06-24 19:55:43 +0200

    test1

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-from fastapi import  FastAPI
+from fastapi import  FastAPI, Request
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"hello": "juden"}
+@app.get("/", include_in_schema=False)
+async def index():
+    response = RedirectResponse(url="/docs")
+    return response
+
+@app.post("/register")
+def register():
+    return {"register": "me"}
+
+@app.post("/login")
+def login(request:Request):
+    client_ip = request.client
+    return {"hello": client_ip}
+
+@app.post("/logout")
+def logout():
+    return {"logout": "me"}
+
+@app.post("/add")
+def add():
+    return {"add": "me"}
+
+@app.post("/remove")
+def remove():
+    return {"remove": "me"}
+
+@app.post("/edit")
+def edit():
+    return {"edit": "me"}
